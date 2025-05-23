1758fd0d9cf14d4f55055bda25ecf8ab75229fcb
Anurag Shanu <s.anurag321@rediffmail.com>
2025-05-23 21:28:42 +0530

    changes after building apk

---
+++ android/generate-keystore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create the keystore file
+keytool -genkeypair -v \
+  -keystore app/release.keystore \
+  -alias chatter-app \
+  -keyalg RSA \
+  -keysize 2048 \
+  -validity 10000 \
+  -storepass chatter123 \
+  -keypass chatter123 \
+  -dname "CN=Chatter App, OU=Mobile Development, O=Chatter, L=Your City, S=Your State, C=IN" 
